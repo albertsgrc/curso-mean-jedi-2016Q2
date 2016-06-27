139908a1338eb261dc383ca48d00995906e5672e
Marc <marc.soler.araguas@est.fib.upc.edu>
2016-06-27 18:57:27 +0200

    First commit

---
+++ ejemplos/00_instalacion_entorno/README.md
@@ -0,0 +1,125 @@
+# Instalación del entorno
+
+Este ejemplo consiste únicamente de este readme, con instrucciones para instalar el programario necesario para el curso.
+
+## Instalación MongoDB
+
+**Preferible instalar v3.2.1**
+
+### Mac OS X
+
+La mejor opción para instalar MongoDB en Mac es usar Homebrew (http://brew.sh/), herramienta que os podéis descargar con este comando:
+
+```ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"```
+
+Ahora, para instalar MongoDB:
+
+1. Actualizar Homebrew
+
+        brew update
+
+2. Instalar MongoDB
+
+        brew install mongodb
+
+3. Cuando haya finalizado la instalación, debemos crear un directorio `/data/db` para almacenar las bases de datos. Este es el directorio que usa mongodb por defecto, aunque es configurable.
+
+        sudo mkdir /data/db
+
+4. Luego hay que asignarle los permisos para que mongodb pueda abrir y modificar la carpeta
+
+        sudo chown $USER /data/db
+
+5. Una vez finalizado esto, podrás iniciar el servidor mongodb con el comando
+
+        mongod
+
+   Y aparecerá un mensaje del tipo *waiting for connections on port 27017* si todo ha ido bien.
+
+### Windows
+
+1. Ir a https://www.mongodb.org/downloads y bajar el paquete adecuado
+
+   Una vez finalizada la instalación, tendrás una carpeta `C:\mongodb` con los ejecutables de mongodb.
+
+2. Crear la carpeta `C:\data\db`
+
+3. Ir a `C:\mongodb\bin` y ejecutar mongod. Si todo ha ido bien aparecerá un mensaje del tipo *waiting for connections on port 27017*.
+
+### Linux Ubuntu
+
+Abrir un Terminal y ejecutar los siguientes comandos:
+
+1. Importar la clave pública para el gestor de paquetes del sistema:
+
+        sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+
+2. Crear un fichero de lista para MongoDB:
+
+        echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+
+3. Actualizar la base de datos de paquetes local:
+
+        sudo apt-get update
+
+4. Instalar los paquetes de MongoDB:
+
+        sudo apt-get install -y mongodb-org
+
+5. Ahora, para iniciar el servidor de mongodb solo tendrás que escribir:
+
+        sudo service mongod start
+
+   Y si todo ha ido bien, aparecerá un mensaje del tipo:
+
+        mongod start/running, process XXXX
+
+### Otras distribuciones Linux
+
+Sigue uno de estos tutoriales, en función de tu distribución:
+
+- [Install on Red Hat](https://docs.mongodb.org/v2.6/tutorial/install-mongodb-on-red-hat/)
+- [Install on Debian](https://docs.mongodb.org/v2.6/tutorial/install-mongodb-on-debian/)
+- [Install on Other Linux Systems](https://docs.mongodb.org/v2.6/tutorial/install-mongodb-on-linux/)
+
+## Instalación Node.js
+
+**Preferible instalar versión LTS (v4.2.6)**
+
+### Mac OS X
+
+Del mismo modo que con MongoDB, se puede usar Homebrew para instalar Node:
+
+    brew install node
+
+Y esto es todo.
+
+### Windows
+
+Ir a http://nodejs.org/#download y bajar el instalador de Windows.
+
+Instalarlo y hecho.
+
+### Linux Ubuntu
+
+1. Abrir un terminal y escribir:
+
+        curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+        sudo apt-get install -y nodejs
+
+2. Para poder iniciar node indiferentemente con los comandos `node` y `nodejs`, lo cual es necesario para algunos paquetes de node, escribe:
+
+        sudo apt-get install nodejs-legacy
+
+### Otras distribuciones Linux
+
+Seguir uno de los tutoriales en este [link](https://nodejs.org/en/download/package-manager/), en función de tu distribución.
+
+## Jetbrains WebStorm IDE (Opcional)
+
+Se recomienda usar el IDE WebStorm (basado en IntellijIDEA) para desarrollar proyectos web.
+
+Lo puedes bajar desde https://www.jetbrains.com/webstorm/
+
+Si te registras con tu cuenta de la UPC, o una cuenta .edu, tendrás acceso gratuito para finalidades educativas.
+Sino, puedes usar el período de prueba de 30 días.
---
+++ ejercicios/01_mongodb/salida_esperada.js
@@ -0,0 +1,389 @@
+// Salida operaciones de consulta
+
+// 2.1
+{
+    "_id" : ObjectId("56a2a37b43e14e025b742bba"),
+    "nombre" : "Fulgencio",
+    "edad" : 16,
+    "departamento" : "RRHH"
+}
+{
+    "_id" : ObjectId("56a2a3bb43e14e025b742bbb"),
+    "nombre" : "Maria",
+    "edad" : 25,
+    "departamento" : "Marketing"
+}
+{
+    "_id" : ObjectId("56a2a3ef43e14e025b742bbc"),
+    "nombre" : "Albert",
+    "edad" : 23,
+    "departamento" : "Marketing",
+    "estado_animo" : "contento",
+    "habilidades" : [ ]
+}
+{
+    "_id" : ObjectId("56a2a42d43e14e025b742bbd"),
+    "nombre" : "Gabriel",
+    "edad" : 30,
+    "departamento" : "Formación",
+    "estado_animo" : "triste",
+    "habilidades" : [
+        "c",
+        "mongodb"
+    ]
+}
+{ "_id" : ObjectId("56a2a6d043e14e025b742bbe"), "nombre" : "h4x0r" }
+{
+    "_id" : ObjectId("56a2a6e743e14e025b742bbf"),
+    "nombre" : "h4x0r",
+    "edad" : 22
+}
+{
+    "_id" : ObjectId("56a2b518473bbc9549c54a11"),
+    "nombre" : "Maria",
+    "edad" : 22,
+    "estado_animo" : "contenta",
+    "departamento" : "Formación",
+    "habilidades" : [
+        "node",
+        "c++",
+        "c",
+        "swift"
+    ]
+}
+{
+    "_id" : ObjectId("56a2b59b473bbc9549c54a12"),
+    "nombre" : "Sergio",
+    "edad" : 35,
+    "esta_casado" : true,
+    "fecha_ultimo_casamiento" : ISODate("fecha_de_insercion (la que sea)"),
+    "departamento" : "RRHH"
+}
+
+
+// 2.2
+{
+    "_id" : ObjectId("56a2a3bb43e14e025b742bbb"),
+    "nombre" : "Maria",
+    "edad" : 25,
+    "departamento" : "Marketing"
+}
+{
+    "_id" : ObjectId("56a2b518473bbc9549c54a11"),
+    "nombre" : "Maria",
+    "edad" : 22,
+    "estado_animo" : "contenta",
+    "departamento" : "Formación",
+    "habilidades" : [
+        "node",
+        "c++",
+        "c",
+        "swift"
+    ]
+}
+
+
+// 2.3
+{
+    "_id" : ObjectId("56a2a3ef43e14e025b742bbc"),
+    "nombre" : "Albert",
+    "edad" : 23,
+    "departamento" : "Marketing",
+    "estado_animo" : "contento",
+    "habilidades" : [ ]
+}
+{
+    "_id" : ObjectId("56a2b518473bbc9549c54a11"),
+    "nombre" : "Maria",
+    "edad" : 22,
+    "estado_animo" : "contenta",
+    "departamento" : "Formación",
+    "habilidades" : [
+        "node",
+        "c++",
+        "c",
+        "swift"
+    ]
+}
+
+
+// 2.4
+{ "_id" : ObjectId("56a2a42d43e14e025b742bbd"), "nombre" : "Gabriel" }
+{ "_id" : ObjectId("56ae07b6fe8df75783157d45"), "nombre" : "Sergio" }
+
+// 2.5
+{
+    "_id" : ObjectId("56a2a3bb43e14e025b742bbb"),
+    "nombre" : "Maria",
+    "departamento" : "Marketing"
+}
+{
+    "_id" : ObjectId("56a2a3ef43e14e025b742bbc"),
+    "nombre" : "Albert",
+    "departamento" : "Marketing",
+    "estado_animo" : "contento",
+    "habilidades" : [ ]
+}
+{
+    "_id" : ObjectId("56a2a6e743e14e025b742bbf"),
+    "nombre" : "h4x0r"
+}
+{
+    "_id" : ObjectId("56a2b518473bbc9549c54a11"),
+    "nombre" : "Maria",
+    "estado_animo" : "contenta",
+    "departamento" : "Formación",
+    "habilidades" : [
+        "node",
+        "c++",
+        "c",
+        "swift"
+    ]
+}
+
+
+// 2.6
+{
+    "_id" : ObjectId("56a2b59b473bbc9549c54a12"),
+    "nombre" : "Sergio",
+    "edad" : 35,
+    "esta_casado" : true,
+    "fecha_ultimo_casamiento" : ISODate("fecha_de_insercion (la que sea)"),
+    "departamento" : "RRHH"
+}
+{
+    "_id" : ObjectId("56a2a42d43e14e025b742bbd"),
+    "nombre" : "Gabriel",
+    "edad" : 30,
+    "departamento" : "Formación",
+    "estado_animo" : "triste",
+    "habilidades" : [
+        "c",
+        "mongodb"
+    ]
+}
+{
+    "_id" : ObjectId("56a2a3bb43e14e025b742bbb"),
+    "nombre" : "Maria",
+    "edad" : 25,
+    "departamento" : "Marketing"
+}
+{
+    "_id" : ObjectId("56a2a3ef43e14e025b742bbc"),
+    "nombre" : "Albert",
+    "edad" : 23,
+    "departamento" : "Marketing",
+    "estado_animo" : "contento",
+    "habilidades" : [ ]
+}
+{
+    "_id" : ObjectId("56a2a6e743e14e025b742bbf"),
+    "nombre" : "h4x0r",
+    "edad" : 22
+}
+{
+    "_id" : ObjectId("56a2b518473bbc9549c54a11"),
+    "nombre" : "Maria",
+    "edad" : 22,
+    "departamento" : "Formación",
+    "estado_animo" : "contenta",
+    "habilidades" : [
+        "node",
+        "c++",
+        "c",
+        "swift"
+    ]
+}
+{
+    "_id" : ObjectId("56a2a37b43e14e025b742bba"),
+    "nombre" : "Fulgencio",
+    "edad" : 16,
+    "departamento" : "RRHH"
+}
+{ "_id" : ObjectId("56a2a6d043e14e025b742bbe"), "nombre" : "h4x0r" }
+
+
+// 2.7
+{
+    "_id" : ObjectId("56a2a42d43e14e025b742bbd"),
+    "nombre" : "Gabriel",
+    "edad" : 30,
+    "departamento" : "Formación",
+    "estado_animo" : "triste",
+    "habilidades" : [
+        "c",
+        "mongodb"
+    ]
+}
+{
+    "_id" : ObjectId("56a2a6e743e14e025b742bbf"),
+    "nombre" : "h4x0r",
+    "edad" : 22
+}
+{
+    "_id" : ObjectId("56a2b518473bbc9549c54a11"),
+    "nombre" : "Maria",
+    "edad" : 22,
+    "estado_animo" : "contenta",
+    "departamento" : "Formación",
+    "habilidades" : [
+        "node",
+        "c++",
+        "c",
+        "swift"
+    ]
+}
+{
+    "_id" : ObjectId("56a2b59b473bbc9549c54a12"),
+    "nombre" : "Sergio",
+    "edad" : 35,
+    "esta_casado" : true,
+    "fecha_ultimo_casamiento" : ISODate("fecha_de_insercion (la que sea)"),
+    "departamento" : "RRHH"
+}
+
+
+// 2.8
+{
+    "_id" : ObjectId("56a2a37b43e14e025b742bba"),
+    "nombre" : "Fulgencio",
+    "edad" : 16,
+    "departamento" : "RRHH"
+}
+{
+    "_id" : ObjectId("56a2a3bb43e14e025b742bbb"),
+    "nombre" : "Maria",
+    "edad" : 25,
+    "departamento" : "Marketing"
+}
+{
+    "_id" : ObjectId("56a2a42d43e14e025b742bbd"),
+    "nombre" : "Gabriel",
+    "edad" : 30,
+    "departamento" : "Formación",
+    "estado_animo" : "triste",
+    "habilidades" : [
+        "c",
+        "mongodb"
+    ]
+}
+
+
+// 2.9
+{
+    "_id" : ObjectId("56a2a42d43e14e025b742bbd"),
+    "nombre" : "Gabriel",
+    "edad" : 30,
+    "departamento" : "Formación",
+    "estado_animo" : "triste",
+    "habilidades" : [
+        "c",
+        "mongodb"
+    ]
+}
+
+
+// 2.10
+{
+    "_id" : ObjectId("56a2b5a6473bbc9549c54a14"),
+    "nombre" : "Limpiar el suelo",
+    "encargados" : [ ],
+    "cancelada" : true
+}
+
+// 2.11
+{
+    "_id" : ObjectId("56a2a3ef43e14e025b742bbc"),
+    "nombre" : "Albert",
+    "edad" : 23,
+    "departamento" : "Marketing",
+    "estado_animo" : "contento",
+    "habilidades" : [ ]
+}
+
+// 2.12
+(No debería salir nada)
+
+
+// 2.13
+{
+    "_id" : ObjectId("56a2b59b473bbc9549c54a12"),
+    "nombre" : "Sergio",
+    "edad" : 35,
+    "esta_casado" : true,
+    "fecha_ultimo_casamiento" : ISODate("fecha_de_insercion (la que sea)"),
+    "departamento" : "RRHH"
+}
+
+
+// Salida de .find().pretty() al haber finalizado todas las operaciones
+
+// Colección usuarios
+{
+    "_id" : ObjectId("56a2a37b43e14e025b742bba"),
+    "nombre" : "José",
+    "edad" : 16,
+    "departamento" : "RRHH",
+    "estado_animo" : "radiante"
+}
+{
+    "_id" : ObjectId("56a2a3bb43e14e025b742bbb"),
+    "nombre" : "Maria",
+    "edad" : 25,
+    "departamento" : "Marketing",
+    "estado_animo" : "radiante"
+}
+{
+    "_id" : ObjectId("56a2a3ef43e14e025b742bbc"),
+    "nombre" : "Albert",
+    "edad" : 23,
+    "departamento" : "Marketing",
+    "estado_animo" : "radiante",
+    "habilidades" : [ ]
+}
+{
+    "_id" : ObjectId("56a2b518473bbc9549c54a11"),
+    "nombre" : "Maria",
+    "edad" : 22,
+    "estado_animo" : "radiante",
+    "departamento" : "Formación",
+    "habilidades" : [
+        "node",
+        "c++",
+        "c",
+        "swift"
+    ]
+}
+{
+    "_id" : ObjectId("56a2a6d043e14e025b742bbe"),
+    "nombre" : "h4x0r",
+    "estado_animo" : "radiante" 
+    // Si tiene atributo edad: 22 también es válido
+}
+
+
+// Colección tareas
+{
+    "_id" : ObjectId("56a2b5a1473bbc9549c54a13"),
+    "nombre" : "Arreglar la bici",
+    "encargados" : [
+        {
+            "nombre" : "Fabian",
+            "obligado" : true
+        },
+        {
+            "nombre" : "Peter",
+            "obligado" : false
+        },
+        {
+            "nombre" : "Oscar",
+            "obligado" : true
+        }
+    ]
+}
+{
+    "_id" : ObjectId("56a2b5a6473bbc9549c54a14"),
+    "nombre" : "Limpiar el suelo",
+    "encargados" : [ ],
+    "cancelada" : true
+}
